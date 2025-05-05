3a7a2b4f2501b76aa427f27d481b26826cad04e1
lait <lait.zhang@gmail.com>
2025-05-05 23:53:38 +0800

    feat(oss): 集成腾讯云对象存储
    - 在 application-oss.yml 中添加腾讯云 OSS 配置
    - 更新 OssService 类，使用腾讯云 OSS客户端
    - 在 pom.xml 中添加 rade-core 和 oss-tencent 依赖
    - 新增 log4j2 配置文件和属性文件

--- demo-oss/src/main/java/org/dows/rade/oss/HraAdminApplication.java
+++ demo-oss/src/main/java/org/dows/rade/oss/HraAdminApplication.java
@@ -1,9 +1,11 @@
 package org.dows.rade.oss;
 
+import com.tangzc.mybatisflex.autotable.MybatisFlexAutoTableAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"org.dows.rade.oss"})
+@SpringBootApplication(scanBasePackages = {"org.dows.rade.oss"},exclude = {DataSourceAutoConfiguration.class, MybatisFlexAutoTableAutoConfig.class})
 //@Import(LocalOssConfiguration.class)
 public class HraAdminApplication {
 
--- demo-oss/src/main/java/org/dows/rade/oss/local/OssService.java
+++ demo-oss/src/main/java/org/dows/rade/oss/local/OssService.java
@@ -2,7 +2,7 @@ package org.dows.rade.oss.local;
 
 import lombok.RequiredArgsConstructor;
 import org.dows.rade.oss.OssInfo;
-import org.dows.rade.oss.minio.MinioOssClient;
+import org.dows.rade.oss.tencent.TencentOssClient;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -12,21 +12,21 @@ import java.io.FileNotFoundException;
 @RequiredArgsConstructor
 public class OssService {
 
-    // private final TencentOssClient tencentOssClient;
+     private final TencentOssClient tencentOssClient;
 
-    private final MinioOssClient minioOssClient;
+    //private final MinioOssClient minioOssClient;
 
     //    private final List<LocalOssClient> localOssClients;
     public void uoload() {
         //OssInfo info = tencentOssClient.upLoad(new File(System.getProperty("user.dir") + File.separator + "1.png"), "2.png", false);
-        OssInfo info = minioOssClient.upLoad(new File(System.getProperty("user.dir") + File.separator + "1.png"), "2.png", true);
+        OssInfo info = tencentOssClient.upLoad(new File(System.getProperty("user.dir") + File.separator + "1.png"), "2.png", true);
 
 
     }
 
     public void download() throws FileNotFoundException {
         //tencentOssClient.downLoadCheckPoint(System.getProperty("user.dir") + "dows-demo" + File.separator + "demo-oss" + File.separator + "1.png", "2.png");
-        minioOssClient.downLoadCheckPoint(System.getProperty("user.dir")  + File.separator + "3.png", "2.png");
+        tencentOssClient.downLoadCheckPoint(System.getProperty("user.dir")  + File.separator + "3.png", "2.png");
     }
 
     public static void main(String[] args) {
